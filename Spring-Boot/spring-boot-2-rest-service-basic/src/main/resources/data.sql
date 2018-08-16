ce978737094b1cdbfc2f1a9f6b9ccdb5856e87e2
ananwoy007@gmail.com <ananandi@DIN55001286.corp.capgemini.com>
2018-08-16 15:04:59 +0530

    added new entries

--- Spring-Boot/spring-boot-2-rest-service-basic/src/main/resources/data.sql
+++ Spring-Boot/spring-boot-2-rest-service-basic/src/main/resources/data.sql
@@ -3,3 +3,9 @@ values(10001,'Ananwoy', 'E1234567');
 
 insert into student
 values(10002,'Roshni', 'A1234568');
+
+insert into student
+values(10003,'Arpit', 'P1234567');
+
+insert into student
+values(10004,'Monika', 'Z1234568');
